35bf8a363ea0a5e67d5bb3a5440244136e998ae4
Johan Friis <johan@slowmail.it>
2025-02-09 14:36:15 +0100

    add keycodes

---
+++ include/zmk-helpers/keycodes.h
@@ -0,0 +1,129 @@
+#define XXXXXXXXXXX &none
+#define ___________ &trans
+#define vvvvvvvvvvv &none  // use when illustrating held key on a layer
+
+#define _____A_____ &kp A
+#define _____B_____ &kp B
+#define _____C_____ &kp C
+#define _____D_____ &kp D
+#define _____E_____ &kp E
+#define _____F_____ &kp F
+#define _____G_____ &kp G
+#define _____H_____ &kp H
+#define _____I_____ &kp I
+#define _____J_____ &kp J
+#define _____K_____ &kp K
+#define _____L_____ &kp L
+#define _____M_____ &kp M
+#define _____N_____ &kp N
+#define _____O_____ &kp O
+#define _____P_____ &kp P
+#define _____Q_____ &kp Q
+#define _____R_____ &kp R
+#define _____S_____ &kp S
+#define _____T_____ &kp T
+#define _____U_____ &kp U
+#define _____V_____ &kp V
+#define _____W_____ &kp W
+#define _____X_____ &kp X
+#define _____Y_____ &kp Y
+#define _____Z_____ &kp Z
+
+#define _____0_____ &kp N0
+#define _____1_____ &kp N1
+#define _____2_____ &kp N2
+#define _____3_____ &kp N3
+#define _____4_____ &kp N4
+#define _____5_____ &kp N5
+#define _____6_____ &kp N6
+#define _____7_____ &kp N7
+#define _____8_____ &kp N8
+#define _____9_____ &kp N9
+
+#define _BACKSLASH_ &kp BACKSLASH //
+#define ___COMMA___ &kp COMMA //
+#define ___COLON___ &kp COLON //
+#define ____DOT____ &kp DOT //
+#define ___EQUAL___ &kp EQUAL //
+#define ___GRAVE___ &kp GRAVE //
+#define ___MINUS___ &kp MINUS //
+#define ___SQUOT___ &kp SINGLE_QUOTE //
+#define ___DQUOT___ &kp DOUBLE_QUOTES //
+#define _SEMICOLON_ &kp SEMICOLON //
+#define ___SLASH___ &kp SLASH //
+#define ____AMP____ &kp AMPERSAND //
+#define ___UNDRS___ &kp UNDERSCORE //
+#define ___PLUS____ &kp PLUS //
+#define ___PIPE____ &kp PIPE //
+#define ___STAR____ &kp STAR
+#define ___HASH____ &kp HASH
+#define __DOLLAR___ &kp DOLLAR
+#define __ATSIGN___ &kp AT_SIGN
+#define ___EXCLM___ &kp EXCLAMATION //
+#define ___QUEST___ &kp QUESTION //
+#define __PERCENT__ &kp PERCENT
+#define ___CARET___ &kp CARET //
+#define ___TILDE___ &kp TILDE //
+
+#define ___LBRCK___ &kp LEFT_BRACKET      // [
+#define ___LBRCE___ &kp LEFT_BRACE        // {
+#define ___LPARN___ &kp LEFT_PARENTHESIS  // (
+#define ___LANGB___ &kp LESS_THAN         // <
+#define ___RBRCK___ &kp RIGHT_BRACKET     // ]
+#define ___RBRCE___ &kp RIGHT_BRACE       // }
+#define ___RPARN___ &kp RIGHT_PARENTHESIS // )
+#define ___RANGB___ &kp GREATER_THAN      // >
+
+#define _BACKSPACE_ &kp BSPC
+#define ____DEL____ &kp DELETE
+#define ___ENTER___ &kp ENTER
+#define ____ESC____ &kp ESCAPE
+#define ___SPACE___ &kp SPACE
+#define ____TAB____ &kp TAB
+
+#define ___HOME____ &kp HOME
+#define ____END____ &kp END
+#define __PAGE_UP__ &kp PG_UP
+#define _PAGE_DOWN_ &kp PG_DN
+#define ____INS____ &kp INS
+
+#define ___LEFT____ &kp LEFT
+#define ___RIGHT___ &kp RIGHT
+#define ____UP_____ &kp UP
+#define ___DOWN____ &kp DOWN
+
+#define ____F01____ &kp F1
+#define ____F02____ &kp F2
+#define ____F03____ &kp F3
+#define ____F04____ &kp F4
+#define ____F05____ &kp F5
+#define ____F06____ &kp F6
+#define ____F07____ &kp F7
+#define ____F08____ &kp F8
+#define ____F09____ &kp F9
+#define ____F10____ &kp F10
+#define ____F11____ &kp F11
+#define ____F12____ &kp F12
+
+#define ____CPY____ &kp LG(C)
+#define ____CUT____ &kp LG(X)
+#define ___PASTE___ &kp LG(V)
+#define ___SLECT___ &kp LG(A)
+#define ___UN_DO___ &kp LG(Z)
+#define ___RE_DO___ &kp LG(LS(Z))
+
+#define ___BOOTL___ &bootloader
+
+#undef LCTL
+#undef RCTL
+
+#define LCTL LCTRL
+#define RCTL RCTRL
+#define LOPT LALT
+#define ROPT RALT
+#define MOD_LOPT MOD_LALT
+#define MOD_ROPT MOD_RALT
+
+#define ___SHIFT___ &kp LSFT
+#define ___LSHFT___ &kp LSFT
+#define ___RSHFT___ &kp RSFT
